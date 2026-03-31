095a6913cb5088e581432806644f68afbc165e42
Sayer-Gage <148849939+Sayer-Gage@users.noreply.github.com>
2026-03-31 12:47:33 -0600

    Update PlotAirfoil.m

--- PlotAirfoil.m
+++ PlotAirfoil.m
@@ -1,7 +1,7 @@
 function PlotAirfoil(x_b,y_b,y_c)
 %This is just a plot function. As we need more complexity we will add it
 %here. For now it just simplifies calls.
-plot(x_b,y_b)
+plot(x_b,y_b,"Marker",".")
 plot(y_c,'-b')
 
 
