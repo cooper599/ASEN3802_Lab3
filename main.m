699016030585046cc22a40bd9493e7da082ad0de
cooper599 <cowa7997@colorado.edu>
2026-03-31 12:19:13 -0600

    Add files via upload

--- main.m
+++ main.m
@@ -2,9 +2,28 @@
 % Task 1: Create NACA Airfoil Generator, Plot Results
 % Task 2: ...
 % Task 3: ...
-% Author : Cooper, Nathan
+% Author: Cooper, Nathan
 % Date: Mar 31, 2026
 clc; clear all; clc;
+
+x = 1:0.1:10;
+m = 0.04; p = 0.4; t = 0.15; c = 10; 
+
+
+y_t = (t/0.2)*c * (0.2969*sqrt(x/c) - 0.3515*(x/c).^2 + 0.2843*(x/c).^3 - 0.1036*(x/c).^4);
+
+
+for i = 1:length(x)
+    if x(i) < p*c
+
+        y_c(i) = m*x(i)/p^2 * (2*p-x(i)/c);
+
+    elseif x(i) >= p*c
+
+        y_c(i) = m*(c-x(i))/(1-p)^2 * (1+(x(i)/c)-(2*p));
+
+    end
+end
 
 function [m,p,t] = extractAirfoilData(airfoilCode)
     digits = airfoilCode - '0';
