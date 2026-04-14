438dd2172104bca345648f1a750abe62bb18f03d
Sayer-Gage <148849939+Sayer-Gage@users.noreply.github.com>
2026-04-14 12:55:39 -0600

    Update ZeroLiftAoA.m
    fixed port errors

--- ZeroLiftAoA.m
+++ ZeroLiftAoA.m
@@ -9,35 +9,10 @@ integrand = Slope .* (cos(theta_0) - 1);
 
 Integral = trapz(theta_0,integrand);
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-ZeroLiftAoA = (180/pi)*((-1/pi) .* Integral);
 
-%There is a sign error somewhere in here********
-
-end
-=======
 ZeroLiftAoA = -(180/pi)*((-1/pi) .* Integral);
 
 %There is a sign error somewhere in here
 %I put a negative sign above in the last line, but there's no justification
 %for it other than it fixes the sign error.
 end
->>>>>>> 6627471ffcd281f833de31d502975be6c03c81d4
-=======
-ZeroLiftAoA = -(180/pi)*((-1/pi) .* Integral);
-
-%There is a sign error somewhere in here********
-%I put a negative sign above in the last line, but there's no justification
-%for it other than it fixes the sign error.
-end
->>>>>>> Stashed changes
-=======
-ZeroLiftAoA = -(180/pi)*((-1/pi) .* Integral);
-
-%There is a sign error somewhere in here********
-%I put a negative sign above in the last line, but there's no justification
-%for it other than it fixes the sign error.
-end
->>>>>>> Stashed changes
